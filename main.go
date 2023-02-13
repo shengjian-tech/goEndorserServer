eeb8b1e062a1d278fb367aca982651e08f44a6ac
springrain <chunanyong@163.com>
2023-02-13 10:42:52 +0800

    修改文件接受大小

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ const (
 	//服务端server.yaml设置 endorserHosts 为本服务地址,设置endorserModule: "proxy"   enableEndorser: true
 	address string = ":8848"
 	// 使用 pb.XchainClient 连接xuperchain服务,发起事务处理
-	xuperChainHost string = "192.168.1.111:37101"
+	xuperChainHost string = "192.168.1.121:37108"
 	// defaultKeyPath 用于签名的证书路径,需要和客户端sdk.yaml中的complianceCheckEndorseServiceAddr保持一致
 	// 最好和节点证书隔离,如果使用同一份证书,使用SDK时会出现异常
 	defaultKeyPath = "./data/endorser/keys/"
@@ -29,13 +29,14 @@ func main() {
 		log.Fatalf("net.Listen err: %v", err)
 	}
 	log.Println(address + " net.Listing...")
-
+	maxSize := 200 * 1024 * 1024
 	// 新建gRPC的服务端实例
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(maxSize), grpc.MaxSendMsgSize(maxSize))
+	diaOpt := grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(maxSize), grpc.MaxCallSendMsgSize(maxSize))
 
 	grpcOpts := []grpc.DialOption{}
 	//grpcOpts = append(grpcOpts, grpc.WithInsecure())
-	grpcOpts = append(grpcOpts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	grpcOpts = append(grpcOpts, grpc.WithTransportCredentials(insecure.NewCredentials()), diaOpt)
 	conn, err := grpc.Dial(xuperChainHost, grpcOpts...)
 	if err != nil {
 		log.Fatalf("grpc.Dial conn err: %v", err)
