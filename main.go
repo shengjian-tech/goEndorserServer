9bb90fc390b2a470b145f75de502ca146d365149
springrain <chunanyong@163.com>
2023-02-13 15:29:42 +0800

    修改注释

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ const (
 	//服务端server.yaml设置 endorserHosts 为本服务地址,设置endorserModule: "proxy"   enableEndorser: true (非必须,主要是 客户端 sdk.yaml 的配置)
 	address string = ":8848"
 	// 使用 pb.XchainClient 连接xuperchain服务,发起事务处理
-	xuperChainHost string = "192.168.1.121:37108"
+	xuperChainHost string = "127.0.0.1:37101"
 	// defaultKeyPath 用于签名的证书路径,需要和客户端sdk.yaml中的complianceCheckEndorseServiceAddr保持一致
 	// 最好和节点证书隔离,如果使用同一份证书,使用SDK时会出现异常
 	defaultKeyPath = "./data/endorser/keys/"
 
-	// 如果使用了监管合约,需要在共识配置xuper.json中配置背书服务收费地址(complianceCheckEndorseServiceFeeAddr)的合约账户,示例如下:
+	// 如果使用了监管合约,需要在共识配置xuper.json中配置背书服务收费AK(complianceCheckEndorseServiceFeeAddr)的合约账户,示例如下:
 	/*
 		, "reserved_contracts": [
 		    {
@@ -35,6 +35,9 @@ const (
 			    "account": "XC9999999999999999@xuper"
 		}
 	*/
+	// 节点AK:负责节点通讯
+	// 背书收费AK(complianceCheckEndorseServiceFeeAddr): 需要创建一个合约账号,配置到xuer.json 的 reserved_whitelist,用于白名单收费
+	// 背书签名AK(complianceCheckEndorseServiceAddr): 背书签名,最好使用这个账号创建的合约,发布监管合约,方便设置ACL
 )
 
 func main() {
